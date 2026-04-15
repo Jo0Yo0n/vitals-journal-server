3c24e6a92a21c4f98cf310b347a9e202d9962124
jo0yo0n <dlwndbs10@gmail.com>
2026-04-15 17:33:54 +0900

    fix(auth): defer resource server activation

--- src/main/java/io/github/jo0yo0n/vitalsjournal/config/SecurityConfig.java
+++ src/main/java/io/github/jo0yo0n/vitalsjournal/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import static org.springframework.security.web.servlet.util.matcher.PathPatternR
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -37,8 +36,7 @@ public class SecurityConfig {
                         "/auth/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**")
                     .permitAll()
                     .anyRequest()
-                    .permitAll()) // TODO: 인증/인가 로직 구현 후 수정
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                    .permitAll()); // TODO: 인증/인가 로직 구현 후 수정
 
     return http.build();
   }
