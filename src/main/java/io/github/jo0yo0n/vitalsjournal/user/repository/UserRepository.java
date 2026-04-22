b653e41519fb00e57c5819eb2748a086d54852b4
jo0yo0n <dlwndbs10@gmail.com>
2026-04-22 16:36:12 +0900

    refactor(user): change UserRepository to extend Repository instead of JpaRepository

--- src/main/java/io/github/jo0yo0n/vitalsjournal/user/repository/UserRepository.java
+++ src/main/java/io/github/jo0yo0n/vitalsjournal/user/repository/UserRepository.java
@@ -2,13 +2,15 @@ package io.github.jo0yo0n.vitalsjournal.user.repository;
 
 import io.github.jo0yo0n.vitalsjournal.user.domain.User;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends Repository<User, Long> {
 
   Optional<User> findByEmailAndDeletedAtIsNull(String email);
 
   boolean existsByEmailAndDeletedAtIsNull(String email);
 
   boolean existsByNicknameAndDeletedAtIsNull(String nickname);
+
+  User saveAndFlush(User user);
 }
